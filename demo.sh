921c5a5b964644e3f43976c9fa1f62af5e510148
Dave Syer <david.syer@broadcom.com>
2025-09-18 13:57:29 +0100

    Simple demo including wrapper

---
+++ demo.sh
@@ -0,0 +1,21 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//SOURCES script@scratches
+//JAVA 25
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class Demo {
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> System.out.println("Hello, World!");
+	}
+
+}
+
+void main(String[] args) {
+	SpringScript.run(args);
+}
